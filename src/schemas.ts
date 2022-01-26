d52a57815be482289138e4a365295027bd625a2e
atm <aaronmyatt@gmail.com>
2022-01-26 10:53:46 +0800

    add relationships between schemas

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,6 +1,22 @@
 import { z } from 'zod'
 import { v4 as uuidv4 } from 'uuid'
 
+export const Task = z.object(
+  {
+    id: z.string().default(() => uuidv4()),
+    name: z.string(),
+  },
+)
+
+export const Reward = z.object(
+  {
+    id: z.string().default(() => uuidv4()),
+    name: z.string(),
+    image: z.string().optional(), // as base64
+    tasks: z.record(z.string(), Task).default({}),
+  },
+)
+
 export const Profile = z.object(
   {
     id: z.string().default(() => uuidv4()),
@@ -10,22 +26,14 @@ export const Profile = z.object(
     hobby: z.string().default(''),
     color: z.string(),
     image: z.string().optional(), // as base64
+    rewards: z.record(z.string(), Reward).default({}),
   },
 )
 
-export const Reward = z.object(
-  {
-    id: z.string().default(() => uuidv4()),
-    name: z.string(),
-    image: z.string().optional(), // as base64
-    profile: z.string().uuid(),
-  },
-)
+export const ProfileSummary = Profile.pick({
+  id: true,
+  name: true,
+  image: true,
+})
 
-export const Task = z.object(
-  {
-    id: z.string().default(() => uuidv4()),
-    name: z.string(),
-    reward: z.string().uuid(),
-  },
-)
+export const ProfileSummaries = z.array(ProfileSummary)
