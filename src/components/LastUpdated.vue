15cd3c3dff5f77b94f5a4518c945c8e75a6d0050
atm <aaronmyatt@gmail.com>
2022-02-05 17:16:21 +0800

    a component to display human readable dates

---
+++ src/components/LastUpdated.vue
@@ -0,0 +1,19 @@
+<template>
+  <UseTimeAgo v-slot="{ timeAgo }" :time="new Date(date)">
+    <span class="text-xs text-gray-400">({{ timeAgo }})</span>
+  </UseTimeAgo>
+</template>
+
+<script lang="ts">
+import { UseTimeAgo } from '@vueuse/components'
+
+export default {
+  components: { UseTimeAgo },
+  props: {
+    date: {
+      type: Date,
+      required: true,
+    },
+  },
+}
+</script>
