febb5e093ee2dbd2ad7d48556811c019c5613653
atm <aaronmyatt@gmail.com>
2022-02-05 17:15:51 +0800

    add create/update timestamps

--- src/schemas.ts
+++ src/schemas.ts
@@ -3,6 +3,8 @@ import { v4 as uuidv4 } from 'uuid'
 
 export const Task = z.object(
   {
+    createddate: z.string().default(new Date().toUTCString()),
+    updateddate: z.string().default(new Date().toUTCString()),
     id: z.string().default(() => uuidv4()),
     done: z.boolean().default(false),
     name: z.string(),
@@ -11,10 +13,13 @@ export const Task = z.object(
 
 export const Reward = z.object(
   {
+    createddate: z.string().default(new Date().toUTCString()),
+    updateddate: z.string().default(new Date().toUTCString()),
     id: z.string().default(() => uuidv4()),
     name: z.string(),
     image: z.string().optional(), // as base64
     tasks: z.record(z.string(), Task).default({}),
+    claimed: z.boolean().default(false),
   },
 )
 
